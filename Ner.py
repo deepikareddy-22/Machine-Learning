8d35cb163ab3fb172f1c75f24ec56006648f0ca7
deepikareddy-22 <aluguvellideepikareddy22@gmail.com>
2025-12-19 10:37:47 +0530

    Add files via upload

---
+++ Ner.py
@@ -0,0 +1,15 @@
+# Dictionary with fake data
+fake_data = {
+    "name": "John Doe",
+    "age": 30,
+    "city": "New York",
+    "occupation": "Software Engineer",
+    "hobby": "Photography",
+    "favorite_color": "Blue"
+}
+
+print(fake_data)
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from collections import Counter
